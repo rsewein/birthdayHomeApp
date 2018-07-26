43529a09b797b091384ad1193f7bb17d57a3034c
rsewein <rsewein@gmail.com>
2018-07-26 00:25:49 -0600

    Fixed timer state but, and got timer object into clockjs JSX.

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,8 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     
+    this.timer= 0
+    
     this.state = {
       active: false,
       startDate: moment(),
@@ -40,7 +42,7 @@ export default class App extends Component {
     var  countDownDate = this.state.startDate.toDate().getTime();
     
     // Update the count down every 1 second
-    var x = setInterval(function () {
+    this.timer = setInterval(function () {
       // Get todays date and time
       var now = new Date().getTime();
 
@@ -50,36 +52,32 @@ export default class App extends Component {
       //Time calculations for day, hours, minutes and seconds
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));;
-      var minutes = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60 * 60 * 24)) / 1000);
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       // Output the result in an element with id="demo"
       const time = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
       
-      this.setState({
-//        timeRemaining: {
-//          days,
-//          hours,
-//          minutes,
-//          seconds
-//        }
-        active: this.state.active
-      })
+      const timeRemaining = {
+        days,
+        hours,
+        minutes,
+        seconds
+      };
+      this.setState({ timeRemaining });
 
-      console.log(this.state.timeRemaining)
-      
       // If the count down is over, write some text
       if (distance < 0) {
-        clearInterval(x);
+        clearInterval(this.timer);
         // document.getElementById("demo").innerHTML = "EXPIRED";
       }
-    }, 1000);
+    }.bind(this), 1000);
   }.bind(this)
   
   renderItems = function() {
     if(this.state.active) {
       return [
-        <Clock/>,
+        <Clock timeRemaining={this.state.timeRemaining}/>,
         ChangeDate('Change Date', () => this.setState({ active: false })),
         LargeText('O4/03'),
         <label className="grid__remaining">Remaining until your 21st birthday</label>
